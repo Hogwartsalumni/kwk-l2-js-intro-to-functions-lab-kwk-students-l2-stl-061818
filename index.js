717a4e693c999e004b146e36c993fef9b8b54e69
Scarlett Patton <scarwars05@gmail.com>
2018-06-21 13:54:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+function getUKTime(number) {
+  const currentHour = new Date().getHours() + 6;
+  return currentHour;
+}
+
+//function discountTicket(price) {
+//  const 
+//}
